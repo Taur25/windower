75b575bc62ad2345f3cd94d22531efb078d7cac8
taur <128584214+Taur25@users.noreply.github.com>
2023-10-19 15:07:39 +0900

    Create sendline.lua

---
+++ sendline/sendline.lua
@@ -0,0 +1,94 @@
+-- Windower用のスクリプト
+
+-- 依存ライブラリをロード
+local http = require("socket.http")
+local ltn12 = require("ltn12")
+
+
+-- IFTTTのWebhook設定
+local ifttt_key = "***************" -- ここにIFTTTのWebhookキーを設定
+local event_name = "hogehoge" -- ここにIFTTTで設定したイベント名を設定
+
+local pattern = '(.*トレジャーハンターの効果が.*にアップ.*|.*ビジタント消滅まで残り.*)'
+local permission_lookup = {
+	[148] = true,	--ビジタント
+	[20] = true,	--トレハン
+	}
+
+local message_queue = {}
+
+-- incoming_textイベントハンドラを登録
+windower.register_event("incoming text", function(original, modified, original_mode, modified_mode, blocked)
+
+if permission_lookup[original_mode] then
+
+-- オリジナルのテキストを取得
+	local message = windower.from_shift_jis(original)
+
+	function string.endswith(s, endStr)
+	    return endStr == '' or s:sub(-#endStr) == endStr
+	end
+
+	function removeSuffix(s, suffix)
+	    if string.endswith(s, suffix) then
+	        return s:sub(1, -#suffix-1)
+	    end
+	    return s
+	end
+
+	local endline = string.char(0x7F, 0x31)
+	local message = removeSuffix(message, endline)
+
+	message = message:gsub("[%c]", "") 
+	
+
+	if windower.regex.match(message,pattern) then
+		--print("Matched: " .. message) -- デバッグ用
+		table.insert(message_queue, original_mode .. ':' .. message) -- キューにメッセージを追加
+
+	end
+end
+end)
+
+-- コルーチンでキューのメッセージを定期的に処理
+coroutine.schedule(function()
+	while true do
+		if #message_queue > 0 then
+			local message = table.remove(message_queue, 1) -- キューからメッセージを取り出し
+			--print("Sending: " .. message) -- デバッグ用
+			sendIFTTTWebHook(message)
+		end
+		coroutine.sleep(0.5) -- 0.5秒ごとにキューをチェック
+	end
+end, 0)
+
+function sendIFTTTWebHook(message)
+
+	-- データをJSON形式に変換
+	local json_data = string.format('{"value1":"%s"}', message)
+	--print(json_data)  -- デバッグ用
+	--HTTPリクエストを構成
+	url = "https://maker.ifttt.com/trigger/" .. event_name .. "/with/key/" .. ifttt_key
+	headers = {
+		["Content-Type"] = "application/json",
+		["Content-Length"] = string.len(json_data)
+		}
+
+	-- HTTPリクエストを送信
+	response = {}
+	res, code, response_headers = http.request {
+		url = url,
+		method = "POST",
+		headers = headers,
+		source = ltn12.source.string(json_data),
+		sink = ltn12.sink.table(response)
+		}
+
+--デバッグ用
+--	if res and code == 200 then
+--		print("Successfully sent: " .. message)
+--	else
+--	print("Failed to send: " .. message .. " with code: " .. (code or "nil"))
+--	end
+
+end
